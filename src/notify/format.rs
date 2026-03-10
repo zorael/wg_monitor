e7be2b4b019d4667374da0315d3b62f77ca1282a
JR <zorael@gmail.com>
2026-03-10 23:51:31 +0100

    Fix first-run formatting missing header and not skipping
    ...when it should.

--- src/notify/format.rs
+++ src/notify/format.rs
@@ -17,8 +17,17 @@ pub fn format_generic_message(
     let mut message = String::new();
 
     if ctx.first_run && !ctx.resume {
-        // This is only called if there are actually missing or late peers,
-        // so we don't need to check for whether missing_keys and late_keys are empty here
+        if !strings.first_run_header.is_empty() {
+            message.push_str(&strings.first_run_header);
+            message.push('\n');
+        }
+
+        if strings.first_run_missing.is_empty()
+            || (ctx.missing_keys.is_empty() && ctx.late_keys.is_empty())
+        {
+            return message.replace("\\n", "\n").trim_end().to_owned();
+        }
+
         if !strings.first_run_missing.is_empty() {
             message.push_str(&strings.first_run_missing);
             message.push('\n');
