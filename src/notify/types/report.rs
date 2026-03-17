80c5a1debb139a718245a4adc4031e03abb3160b
JR <zorael@gmail.com>
2026-03-17 16:34:58 +0100

    Fix some missing documentation

--- src/notify/types/report.rs
+++ src/notify/types/report.rs
@@ -4,9 +4,18 @@
 #[allow(unused)]
 #[derive(Default, Debug)]
 pub struct DispatchReport {
+    /// Total number of notifications attempted to be sent.
     pub total: u32,
+
+    /// Number of notifications successfully sent.
     pub successful: u32,
+
+    /// Number of notifications that failed to send.
     pub failed: u32,
+
+    /// Number of notifications that were skipped due to missing message content.
     pub no_message: u32,
+
+    /// Number of notifications that were skipped.
     pub skipped: u32,
 }
