53b01f5689b38ac248183fbdc014c6243a7cefcc
JR <zorael@gmail.com>
2026-03-17 17:25:58 +0100

    Add example notify-send scripts

---
+++ notify-send-to-all-gui.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# $1 contains the composed message
+# $2 contains the path to the peers.txt file, not relevant for this script
+# $3 contains the loop iteration number
+
+icon="network-wireless-disconnected"
+urgency="critical"
+loop_number=$3
+ids=( $(loginctl list-sessions -j | jq -r '.[] | .session') )
+
+if [[ "$loop_number" = "0" ]]; then
+    # run 0
+    summary="WireGuard Monitor: first run"
+else
+    summary="WireGuard Monitor: update"
+fi
+
+for id in "${ids[@]}" ; do
+    [[ $(loginctl show-session $id --property=Type) =~ (wayland|x11) ]] || continue
+
+    user=$(loginctl show-session $id --property=Name --value)
+
+    systemd-run --machine=${user}@.host --user \
+        notify-send \
+            --icon="$icon" \
+            --urgency="$urgency" \
+            "$summary" \
+            "$1"
+done
---
+++ notify-send-to-one.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# $1 contains the composed message
+# $2 contains the path to the peers.txt file, not relevant for this script
+# $3 contains the loop iteration number
+
+# make sure to change the "user" variable to the actual username or user ID
+# of the user you want to send the notification to, e.g. 1000, "bob" or "alice".
+
+icon="network-wireless-disconnected"
+urgency="critical"
+loop_number=$3
+user=1000
+
+if [[ "$loop_number" = "0" ]]; then
+    # run 0
+    summary="WireGuard Monitor: first run"
+else
+    summary="WireGuard Monitor: update"
+fi
+
+systemd-run --machine=${user}@.host --user \
+    notify-send \
+        --icon="$icon" \
+        --urgency="$urgency" \
+        "$summary" \
+        "$1"
