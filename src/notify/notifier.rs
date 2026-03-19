9e62dc1b80490ff884beb67ed5d1afce822657de
JR <zorael@gmail.com>
2026-03-19 15:59:28 +0100

    Don't print success body on Slack/Batsign emit request

--- src/notify/notifier.rs
+++ src/notify/notifier.rs
@@ -60,11 +60,7 @@ impl<B: backend::Backend> super::NotificationSender for Notifier<B> {
         }
 
         match self.backend.emit(ctx, Some(delta), &message) {
-            Ok(Some(response)) => {
-                println!("{response}");
-                super::NotificationResult::Success(message)
-            }
-            Ok(None) => super::NotificationResult::Success(message),
+            Ok(_) => super::NotificationResult::Success(message),
             Err(e) => super::NotificationResult::Failure(e, message),
         }
     }
@@ -81,11 +77,7 @@ impl<B: backend::Backend> super::NotificationSender for Notifier<B> {
         }
 
         match self.backend.emit(ctx, None, &reminder) {
-            Ok(Some(response)) => {
-                println!("{response}");
-                super::NotificationResult::Success(reminder)
-            }
-            Ok(None) => super::NotificationResult::Success(reminder),
+            Ok(_) => super::NotificationResult::Success(reminder),
             Err(e) => super::NotificationResult::Failure(e, reminder),
         }
     }
