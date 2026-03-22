1e4c2ca6ed9f745dedf3702029121864bf80f934
JR <zorael@gmail.com>
2026-03-22 15:41:47 +0100

    Delete utils::strings::escape_json
    We don't seem to need it.
    
    Revert if the situation changes.

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -7,7 +7,7 @@ mod string;
 mod time;
 mod vec;
 
-pub use string::{escape_json, unescape};
+pub use string::unescape;
 pub use time::{datestamp_now, datestamp_of, timestamp_now, timestamp_of};
 pub use vec::{append_vec_difference, trim_vec_of_strings};
 pub use vec::{get_elements_not_in_other_vec, get_vec_difference};
--- src/utils/string.rs
+++ src/utils/string.rs
@@ -1,43 +1,5 @@
-//! Utility functions for string manipulation, including escaping and
-//! unescaping of special characters.
-
-/// Escapes special characters in a string for safe inclusion in JSON, such as
-/// backslashes, quotes, newlines, tabs, and curly braces.
-///
-/// This is important for ensuring that strings are properly formatted when
-/// included in JSON messages, such as those used in notifications.
-///
-/// The following characters are escaped:
-/// - `\` becomes `\\`
-/// - `"` becomes `\"`
-/// - `\n` becomes `\\n`
-/// - `\r` becomes `\\r`
-/// - `\t` becomes `\\t`
-/// - `{` becomes `\\{`
-/// - `}` becomes `\\}`
-///
-/// # Examples
-/// ```rust
-/// let input = "Hello \"world\"!\nThis is a test.\t{Curly braces}";
-/// let escaped = escape_json(input);
-/// assert_eq!(escaped, "Hello \\\"world\\\"!\\nThis is a test.\\t\\{Curly braces\\}");
-/// ```
-///
-/// # Parameters
-/// - `input`: The input string to escape.
-///
-/// # Returns
-/// - A new string with special characters escaped for JSON.
-pub fn escape_json(input: &str) -> String {
-    input
-        .replace('\\', "\\\\")
-        .replace('"', "\\\"")
-        .replace('\n', "\\n")
-        .replace('\r', "\\r")
-        .replace('\t', "\\t")
-        .replace("{", "\\{")
-        .replace("}", "\\}")
-}
+//! Utility functions for string manipulation, including unescaping
+//! of special characters.
 
 /// Unescapes a string that has been escaped for JSON, reversing the escaping
 /// of special characters such as backslashes, quotes, newlines, tabs, and curly
@@ -81,15 +43,6 @@ pub fn unescape(input: &str) -> String {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    /// Tests the `escape_json` function to ensure that special characters are
-    /// properly escaped for JSON formatting.
-    #[test]
-    fn test_escape_json() {
-        let input = "Hello \"world\"!\nThis is a test.\t{Curly braces}";
-        let expected = "Hello \\\"world\\\"!\\nThis is a test.\\t\\{Curly braces\\}";
-        assert_eq!(escape_json(input), expected);
-    }
 
     /// Tests the `unescape` function to ensure that escaped strings are properly
     /// converted back to their original form.
