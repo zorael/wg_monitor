68d345d8e303ce9c85aac970272c5625a14d2370
JR <zorael@gmail.com>
2026-03-27 21:46:51 +0100

    Fix some documentation drift

--- src/main.rs
+++ src/main.rs
@@ -3,36 +3,36 @@
 //!
 //! The main purpose of this is to monitor Internet-connected locations for
 //! power outages, using WireGuard handshakes as a way for sites to phone home.
-//! Each needs an always-on, always-connected computer to act as a WireGuard
+//! Each site needs an always-on, always-online computer to act as a WireGuard
 //! peer, for which something like a
 //! [Raspberry Pi Zero 2W](https://www.raspberrypi.com/products/raspberry-pi-zero-2-w)
-//! is cheap and more than sufficient. (May require [cross-compilation](#cross-compilation).)
+//! is cheap and more than sufficient. (May require cross-compilation.)
 //!
 //! In a hub-and-spoke WireGuard configuration, this should be run on the hub
-//! server, ideally with an additional instance on (at least) one other
+//! server, with an additional instance on at least one other
 //! geographically disconnected peer to monitor the hub. In other configurations,
 //! it can be run on any peer with visibility of other peers, but a secondary
 //! instance monitoring the first is recommended in any setup.
+//! If the hub loses power, it cannot report itself as being lost.
 //!
 //! Peers must have a `PersistentKeepalive` setting in their WireGuard
 //! configuration with a value *comfortably lower* than the peer timeout of
-//! this program. This timeout is **600 seconds** by default, but can be
-//! overridden by modifying a configuration file.
+//! this program. This timeout is **10 minutes** by default.
 //!
 //! Notifications can be sent as
 //! [**Slack**](https://docs.slack.dev/messaging/sending-messages-using-incoming-webhooks)
 //! messages, as short emails via [**Batsign**](https://batsign.me), and/or by
 //! invocation of an [**external command**](#external-command)
-//! (like `notify-send` or `sendmail`).
+//! (like `notify-send`, `wall` or `sendmail`).
 //!
-//! At any given time, peers can be in one of three states:
+//! At any given time, any given peer is in one of three states:
 //!
 //! - **present**: the peer has been seen within the timeout period.
 //! - **lost**: the peer has been seen before but has not been seen within the
 //!   timeout period. It may be referred to as "lost" in some message strings.
 //! - **missing**: the peer has not been seen since the last restart of the VPN.
 //!
-//! As such, peer may be in the following transition states;
+//! As such, peers may be in the following transition states;
 //!
 //! - **now lost**: the peer was present but is now lost.
 //! - **now missing**: the peer was present but is now missing, which is usually
