1f033868dfc9260ef8ad219155dde2d6143a1c7d
JR <zorael@gmail.com>
2026-03-27 23:11:08 +0100

    Deduplicate logic in build_notifiers

--- src/main.rs
+++ src/main.rs
@@ -546,71 +546,64 @@ fn build_notifiers(settings: &settings::Settings) -> Vec<Box<dyn notify::Statefu
         )
     };
 
-    if settings.dry_run {
-        // Use dummy URLs for dry runs so that we can get output for all backends
-        // even if no URLs were configured.
-
-        let (dry_run_slack, dry_run_batsign, dry_run_command) = match (
-            settings.slack.enabled,
-            settings.batsign.enabled,
-            settings.command.enabled,
-        ) {
-            (false, false, false) => {
-                logging::tseprintln!(
-                    &settings.disable_timestamps,
-                    "No backends enabled. Enabling all backends for dry run."
-                );
-                (true, true, true)
-            }
-            other => other,
-        };
-
-        if dry_run_slack {
-            let dummy_slack_urls = vec![defaults::DUMMY_SLACK_URL.to_string()];
-            build_and_push_notifiers(&mut notifiers, &dummy_slack_urls, make_slack_backend, true);
-        }
-
-        if dry_run_batsign {
-            let dummy_batsign_urls = vec![defaults::DUMMY_BATSIGN_URL.to_string()];
-            build_and_push_notifiers(
-                &mut notifiers,
-                &dummy_batsign_urls,
-                make_batsign_backend,
-                true,
+    let (slack_enabled, batsign_enabled, command_enabled) = match (
+        settings.slack.enabled,
+        settings.batsign.enabled,
+        settings.command.enabled,
+    ) {
+        (false, false, false) if settings.dry_run => {
+            logging::tseprintln!(
+                &settings.disable_timestamps,
+                "No backends enabled. Enabling all backends because --dry-run is set."
             );
+            (true, true, true)
         }
+        other => other,
+    };
 
-        if dry_run_command {
-            let dummy_command = vec![defaults::DUMMY_COMMAND.to_string()];
-            build_and_push_notifiers(&mut notifiers, &dummy_command, make_command_backend, true);
-        }
-    } else {
-        if settings.slack.enabled && !settings.slack.urls.is_empty() {
-            build_and_push_notifiers(
-                &mut notifiers,
-                &settings.slack.urls,
-                make_slack_backend,
-                false,
-            );
-        }
+    // Use dummy URLs for dry runs so that we can get output for all (enabled)
+    // backends even if no URLs were configured.
 
-        if settings.batsign.enabled && !settings.batsign.urls.is_empty() {
-            build_and_push_notifiers(
-                &mut notifiers,
-                &settings.batsign.urls,
-                make_batsign_backend,
-                false,
-            )
-        }
+    let slack_urls = match settings.dry_run {
+        true if slack_enabled => vec![defaults::DUMMY_SLACK_URL.to_string()],
+        _ => settings.slack.urls.clone(),
+    };
 
-        if settings.command.enabled && !settings.command.commands.is_empty() {
-            build_and_push_notifiers(
-                &mut notifiers,
-                &settings.command.commands,
-                make_command_backend,
-                false,
-            )
-        }
+    let batsign_urls = match settings.dry_run {
+        true if batsign_enabled => vec![defaults::DUMMY_BATSIGN_URL.to_string()],
+        _ => settings.batsign.urls.clone(),
+    };
+
+    let commands = match settings.dry_run {
+        true if command_enabled => vec![defaults::DUMMY_COMMAND.to_string()],
+        _ => settings.command.commands.clone(),
+    };
+
+    if slack_enabled && !slack_urls.is_empty() {
+        build_and_push_notifiers(
+            &mut notifiers,
+            &slack_urls,
+            make_slack_backend,
+            settings.dry_run,
+        );
+    }
+
+    if batsign_enabled && !batsign_urls.is_empty() {
+        build_and_push_notifiers(
+            &mut notifiers,
+            &batsign_urls,
+            make_batsign_backend,
+            settings.dry_run,
+        )
+    }
+
+    if command_enabled && !commands.is_empty() {
+        build_and_push_notifiers(
+            &mut notifiers,
+            &commands,
+            make_command_backend,
+            settings.dry_run,
+        )
     }
 
     notifiers
