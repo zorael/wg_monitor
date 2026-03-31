91dfa41f3d83e1db95d8e1b9d2ffa71fd372aa8e
JR <zorael@gmail.com>
2026-03-31 19:31:42 +0200

    Add utils::time::fuzzy_datestamp_of

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -8,6 +8,6 @@ mod time;
 mod vec;
 
 pub use string::{plurality, unescape};
-pub use time::{datestamp_now, datestamp_of, timestamp_now, timestamp_of};
+pub use time::{datestamp_now, datestamp_of, fuzzy_datestamp_of, timestamp_now, timestamp_of};
 pub use vec::{append_vec_difference, trim_vec_of_strings};
 pub use vec::{get_elements_not_in_other_vec, get_vec_difference};
--- src/utils/time.rs
+++ src/utils/time.rs
@@ -55,3 +55,37 @@ pub fn timestamp_now() -> String {
 pub fn datestamp_now() -> String {
     datestamp_of(&time::SystemTime::now())
 }
+
+/// Converts a `time::SystemTime` to a human-readable datestamp string in a
+/// fuzzy format that depends on how much time has passed since the given time.
+///
+/// - If more than 3 days have passed, the datestamp will be in the format
+///   "`YYYY-MM-DD`" (not showing the time).
+/// - If more than 12 hours but less than 3 days have passed, the datestamp will
+///   be in the format "`YYYY-MM-DD HH:MM:SS`" (showing both date and time).
+/// - If less than 12 hours have passed, the datestamp will be in the format
+///   "`HH:MM:SS`" (showing only the time).
+///
+/// # Parameters
+/// - `when`: A reference to a `time::SystemTime` instance representing the time
+///   to be converted to a fuzzy datestamp string.
+///
+/// # Returns
+/// A `String` containing the formatted fuzzy datestamp based on how much time has
+/// passed since the given time, in one of the formats described above.
+pub fn fuzzy_datestamp_of(when: &time::SystemTime) -> String {
+    const THREE_DAYS: time::Duration = time::Duration::from_secs(3 * 24 * 3600);
+    const TWELVE_HOURS: time::Duration = time::Duration::from_secs(12 * 3600);
+
+    let now = time::SystemTime::now();
+    let since = now.duration_since(*when).unwrap_or_default();
+    let datetime: chrono::DateTime<chrono::Local> = (*when).into();
+
+    if since > THREE_DAYS {
+        datetime.format("%Y-%m-%d").to_string()
+    } else if since > TWELVE_HOURS {
+        datetime.format("%Y-%m-%d %H:%M:%S").to_string()
+    } else {
+        datetime.format("%H:%M:%S").to_string()
+    }
+}
